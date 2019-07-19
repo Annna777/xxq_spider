2aaadb670d18a1d5a9e7ceb0cae31b9263525c6e
Annna <790845853@qq.com>
2019-07-19 14:29:17 +0800

    update files

--- snake_game/Snake/game_opration.c
+++ snake_game/Snake/game_opration.c
@@ -64,10 +64,7 @@ void OnTimer(HWND hwnd)
 	// 计时器到时以后，蛇移动一步。
 	// 根据以后以后的状态，进行后续处理。
 	switch (SnakeMove())
-<<<<<<< HEAD
 
-=======
->>>>>>> 0e4b4d2a96e6534591b516fb3ff98e8bf735cd4e
 	{
 	// 如果蛇已经死了
 	case SNAKE_KNOCKBORDER:
@@ -78,7 +75,7 @@ void OnTimer(HWND hwnd)
 		// 首先关闭计时器，避免在计时器的驱动下再次调用本函数
 		KillTimer(hwnd, TIMER_ID);
 		// 然后通知玩家，Game Over了，退出进程。
-		MessageBox(0, "Game Over", "Game Over", 0);
+		MessageBox(0, "Snake Died", "Game Over", 0);
 		ExitProcess(0);
 		break;
 	// 如果蛇吃到了食物
@@ -145,12 +142,8 @@ void OnTimer(HWND hwnd)
 	//障碍物也移动一步
 	blocks_move_step();
 	*/
-<<<<<<< HEAD
 	
 	if (rand() % 1000 < 500) // 7%的概率，随机产生障碍。
-=======
-	if (rand() % 1000 < 500) // 2%的概率，随机产生障碍。
->>>>>>> 0e4b4d2a96e6534591b516fb3ff98e8bf735cd4e
 	{
 		gen_block();
 	}
--- snake_game/Snake/ui.c
+++ snake_game/Snake/ui.c
@@ -198,10 +198,7 @@ int WINAPI WinMain(
 
 
 	// 以下进入消息循环。获取消息--翻译消息--分配消息（由窗口的消息处理函数来处理消息）
-<<<<<<< HEAD
 
-=======
->>>>>>> 0e4b4d2a96e6534591b516fb3ff98e8bf735cd4e
 	while ((fGotMessage = GetMessage(&msg, (HWND)NULL, 0, 0)) != 0&& fGotMessage != -1)
 	{
 		TranslateMessage(&msg);
@@ -248,11 +245,7 @@ void GamePaint(HWND hwnd)
 	RECT rect;
 	PGAME_COORD pSnakeBody;
 	PGAME_COORD pWallBody;
-<<<<<<< HEAD
 	LPAUTO_BLOCK pBlockBody;
-=======
-	PGAME_COORD pBlockBody;
->>>>>>> 0e4b4d2a96e6534591b516fb3ff98e8bf735cd4e
 	PGAME_COORD lpFood;
 	int i, snake_size,block_size;
 
@@ -352,11 +345,7 @@ void GamePaint(HWND hwnd)
 	block_size = get_blocks_num();
 	for (i = 0; i < block_size; i++)
 	{
-<<<<<<< HEAD
 		pBlockBody = (LPAUTO_BLOCK)get_block_at(i);
-=======
-		pBlockBody = (PGAME_COORD)get_block_at(i);
->>>>>>> 0e4b4d2a96e6534591b516fb3ff98e8bf735cd4e
 
 		Rectangle(hdcmem,
 			get_block_x(pBlockBody) * CELL_PIXEL + rectBoundary.left,
